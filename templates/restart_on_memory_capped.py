97c556bb1eaf9d64cef6fc2299926a76b3078cbd
Jason Robinson <mail@jasonrobinson.me>
2016-10-30 10:05:11 +0200

    Don't crash restart_on_memory_capped.py on processes that have gone away
    Happened during looping where the process we were investigating had ended already.

--- templates/restart_on_memory_capped.py
+++ templates/restart_on_memory_capped.py
@@ -13,17 +13,21 @@ MEM_MAX_MB = {{ diaspora_restart_on_memory }}
 
 
 for proc in psutil.process_iter():
-    if proc.username == "{{ os_user }}" and " ".join(proc.cmdline).find("sidekiq") > -1:
-        current_mem = proc.get_memory_info().rss/1024/1024
-        if current_mem > MEM_MAX_MB:
-            # Restart!
-            with open("/home/{{ os_user }}/restart_on_memory_capped.log", "a") as logfile:
-                logfile.write("{datetime} | Memory at {mem} MB, restarting...\n".format(
-                    datetime=datetime.datetime.now().isoformat(), mem=current_mem
-                ))
-            # Use `initctl` instead of `service`. The latter gives `unrecognized service` when done in root crontab..
-            os.system("/sbin/initctl restart {{ servicename }}")
-            with open("/home/{{ os_user }}/restart_on_memory_capped.log", "a") as logfile:
-                logfile.write("{datetime} | Restart done\n".format(
-                    datetime=datetime.datetime.now().isoformat()
-                ))
+    try:
+        if proc.username == "{{ os_user }}" and " ".join(proc.cmdline).find("sidekiq") > -1:
+            current_mem = proc.get_memory_info().rss/1024/1024
+            if current_mem > MEM_MAX_MB:
+                # Restart!
+                with open("/home/{{ os_user }}/restart_on_memory_capped.log", "a") as logfile:
+                    logfile.write("{datetime} | Memory at {mem} MB, restarting...\n".format(
+                        datetime=datetime.datetime.now().isoformat(), mem=current_mem
+                    ))
+                # Use `initctl` instead of `service`. The latter gives `unrecognized service` when done in root crontab..
+                os.system("/sbin/initctl restart {{ servicename }}")
+                with open("/home/{{ os_user }}/restart_on_memory_capped.log", "a") as logfile:
+                    logfile.write("{datetime} | Restart done\n".format(
+                        datetime=datetime.datetime.now().isoformat()
+                    ))
+    except psutil.NoSuchProcess:
+        # Process went away while we're looping
+        pass
